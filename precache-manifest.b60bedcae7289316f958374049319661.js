0d26b95f906e27d8c8039dc3bc8cdbd0f9e4bb93
dipin4233 <2018pgcaca83.nitjsr.ac.in>
2020-02-27 11:41:32 +0530

    Updates

---
+++ precache-manifest.b60bedcae7289316f958374049319661.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3da4844d60aeaf1bb1dd872aee50e4c",
+    "url": "/firstreactapp/index.html"
+  },
+  {
+    "revision": "0b34231c371272d4ece0",
+    "url": "/firstreactapp/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "b7532a54bd168ef9103f",
+    "url": "/firstreactapp/static/css/main.c9a8e353.chunk.css"
+  },
+  {
+    "revision": "0b34231c371272d4ece0",
+    "url": "/firstreactapp/static/js/2.1d6c54f3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/firstreactapp/static/js/2.1d6c54f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7532a54bd168ef9103f",
+    "url": "/firstreactapp/static/js/main.9f86c43d.chunk.js"
+  },
+  {
+    "revision": "b5c0cf69ac154c556140",
+    "url": "/firstreactapp/static/js/runtime-main.e157db06.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/firstreactapp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
