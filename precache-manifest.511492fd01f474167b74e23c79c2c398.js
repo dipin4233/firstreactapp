4c525bd8f9fe801cb750f067e9378a35a9f2e2ca
dipin4233 <2018pgcaca83.nitjsr.ac.in>
2020-02-27 16:49:25 +0530

    Updates

--- precache-manifest.511492fd01f474167b74e23c79c2c398.js
+++ precache-manifest.511492fd01f474167b74e23c79c2c398.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e3674d03434834e4e867667af7165287",
+    "revision": "f7748daf285065b34839c45c9c2b147f",
     "url": "/firstreactapp/index.html"
   },
   {
-    "revision": "a43de37d487ee43886a6",
+    "revision": "35152a65d63dd0720d11",
     "url": "/firstreactapp/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "ba853289cbf41295cc0c",
+    "revision": "4cd6a3fbc243b64f8c8c",
     "url": "/firstreactapp/static/css/main.c9a8e353.chunk.css"
   },
   {
-    "revision": "a43de37d487ee43886a6",
-    "url": "/firstreactapp/static/js/2.8dcd38b7.chunk.js"
+    "revision": "35152a65d63dd0720d11",
+    "url": "/firstreactapp/static/js/2.486c5b10.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/firstreactapp/static/js/2.8dcd38b7.chunk.js.LICENSE.txt"
+    "url": "/firstreactapp/static/js/2.486c5b10.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "ba853289cbf41295cc0c",
-    "url": "/firstreactapp/static/js/main.c649250a.chunk.js"
+    "revision": "4cd6a3fbc243b64f8c8c",
+    "url": "/firstreactapp/static/js/main.b302b004.chunk.js"
   },
   {
     "revision": "b5c0cf69ac154c556140",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/firstreactapp/precache-manifest.34827817655f2ac7c167f6bf97e3c89a.js"
+  "/firstreactapp/precache-manifest.511492fd01f474167b74e23c79c2c398.js"
 );
 
 self.addEventListener('message', (event) => {
